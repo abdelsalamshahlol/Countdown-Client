3a2cca511b9161c999d7cad96c37a9694a396348
alijalal-sys <ajmabilal@gmail.com>
2020-01-15 20:01:29 +0100

    signup component done the basic functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 // Components
 import {AppComponent} from './app.component';
@@ -10,12 +12,14 @@ import {ProductComponent} from './pages/product/product.component';
 import {NavbarComponent} from './components/UI/navbar/navbar.component';
 import {FooterComponent} from './components/UI/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 // Routes array
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'product', component: ProductComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 // Testing jQuery
@@ -30,10 +34,13 @@ console.log(`jQuery version: ${$.fn.jquery}`);
     NavbarComponent,
     FooterComponent,
     SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MustMatch } from '../../helpers/must-match.validator';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted: boolean = false;
+  userData: any = [];
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
   }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+    this.userData.push(this.registerForm.value);
+    console.log(this.userData)
+    // this.userData.push(JSON.stringify(this.registerForm.value))
+    // alert('SUCCESS!! :) \n\n' + JSON.stringify(this.registerForm.value, null, 4));
+
+  }
+
+
 
 }
