e4eeaac5fb2c068f29aa16b2d4b212088d5efc4d
FoozBarakat <reachfooz@gmail.com>
2020-01-16 15:15:22 +0100

    contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 // Components
-import {AppComponent} from './app.component';
-import {IndexComponent} from './pages/index/index.component';
-import {ProductComponent} from './pages/product/product.component';
-import {NavbarComponent} from './components/UI/navbar/navbar.component';
-import {FooterComponent} from './components/UI/footer/footer.component';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ProductComponent } from './pages/product/product.component';
+import { NavbarComponent } from './components/UI/navbar/navbar.component';
+import { FooterComponent } from './components/UI/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 // Routes array
 const routes: Routes = [
-  {path: '', component: IndexComponent},
-  {path: 'product', component: ProductComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent}
+  { path: '', component: IndexComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 // Testing jQuery
@@ -34,7 +36,8 @@ console.log(`jQuery version: ${$.fn.jquery}`);
     NavbarComponent,
     FooterComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
