c6da71880b50acb78fd313b1f85b73b8a15906e9
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2020-01-17 12:13:06 +0200

    added another prop for navbar to detach value and fixed contactus page style

--- src/app/components/UI/navbar/navbar.component.ts
+++ src/app/components/UI/navbar/navbar.component.ts
@@ -8,7 +8,8 @@ import {Component, OnInit, Input} from '@angular/core';
 export class NavbarComponent implements OnInit {
   @Input() isFixed = true;
   @Input() isTransparent = true;
-  
+  @Input() detachOnScroll = 200;
+
   constructor() {
   }
 
