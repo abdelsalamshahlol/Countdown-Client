267faf6284683029f83ea68b80452db26e55e830
alijalal-sys <ajmabilal@gmail.com>
2020-01-17 18:26:20 +0100

    Login signup Buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {fader} from './helpers/route-animations';
 import {RouterOutlet, Router} from '@angular/router';
 import { AuthenticationService } from './services';
