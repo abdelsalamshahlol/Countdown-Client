45e4f07e52ae461e098428ece649c4f3a9901d80
abdelsalamshahlol <abdulsalamshahlol@gmail.com>
2020-01-18 11:00:18 +0200

    filter by category finalized

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -41,58 +41,7 @@ export class ProductComponent implements OnInit {
 
   unfilteredProducts;
 
-  // products: Product[];
-  products = [
-    {
-      id: "1",
-      name: "Product 1",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: 'https://source.unsplash.com/1600x900/?product',
-      category: "Sport",
-      last_auction_price: 800,
-      value: 620,
-      end_date: "23:00:00"
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: 'https://source.unsplash.com/1600x900/?product',
-      category: "arts",
-      last_auction_price: 800,
-      value: 620,
-      end_date: "23:00:00"
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: 'https://source.unsplash.com/1600x900/?product',
-      category: "Sport",
-      last_auction_price: 800,
-      value: 620,
-      end_date: "23:00:00"
-    },
-    {
-      id: 5,
-      name: "Product 4",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: 'https://source.unsplash.com/1600x900/?product',
-      category: "Sport",
-      last_auction_price: 800,
-      value: 620,
-      end_date: "23:00:00"
-    },
-    {
-      id: 6,
-      name: "Product 5",
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: 'https://source.unsplash.com/1600x900/?product',
-      category: "Sport",
-      last_auction_price: 800,
-      value: 620,
-      end_date: "23:00:00"
-    }]
+  products: Product[];
 
   resetForm(): void {
     this.filterForm.reset({sliderControl: [0, 0]});
@@ -119,7 +68,7 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {
     this.productService.getAllProducts().subscribe(products => {
-      // this.products = products;
+      this.products = products;
       this.unfilteredProducts = this.products.slice();
     });
   }
